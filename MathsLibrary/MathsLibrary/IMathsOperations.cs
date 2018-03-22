77e5fb1c1bf7929e4da33e604c9f8c0174aa004f
Florian bourniquel <florianbourniquel@gmail.com>
2018-03-22 11:50:45 +0100

    Ajout du TD ABC

---
+++ MathsLibrary/MathsLibrary/IMathsOperations.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace MathsLibrary
+{
+    [ServiceContract]
+    public interface IMathsOperations
+    {
+        [OperationContract]
+        int Add(int num1, int num2);
+        [OperationContract]
+        int Multiply(int num1, int num2);
+        [OperationContract]
+        [FaultContract(typeof(CustomFaultDetails))]
+        int Divide(int num1, int num2);
+    }
+
+    [DataContract]
+    internal class CustomFaultDetails
+    {
+        [DataMember]
+        public int errorId { get; set; }
+        [DataMember]
+        public string error { get; set; }
+    }
+}
